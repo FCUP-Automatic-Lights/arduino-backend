fe6ff20a005eb4dfeec1dbfc6b8a0cd2128c5320
João Paulo <joaolrpaulo@gmail.com>
2017-05-16 00:24:50 +0100

    Final commit
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>

--- lib/Automatic_Lights/Automatic_Lights.h
+++ lib/Automatic_Lights/Automatic_Lights.h
@@ -1,12 +1,10 @@
 #include "Automatic_Lights_Types.h"
 
-void _blink(int);
+void _envAnalysis(int, int);
 void _reset_sensor(SensorInfo*, int);
 void _reset_room(RoomInfo*);
-void _set_color(int, int, int);
 void _deactivate_ir_sensors();
 
-void _debug_connection();
 void _debug_room();
 void _debug_ldr_sensor(RoomInfo*);
 void _debug_ir_sensors(int, SensorInfo*);
--- lib/Automatic_Lights/Automatic_Lights_Types.h
+++ lib/Automatic_Lights/Automatic_Lights_Types.h
@@ -2,16 +2,20 @@
  * Sensor Information
  */
 struct SensorInfo {
-    unsigned long timestamp;
-    int status;
-    int mode;
-    int pin;
+  long timestamp;
+  int status;
+  int mode;
+  int pin;
 };
 
 /*
  * Room Information
  */
 struct RoomInfo {
-    int person_count;
-    int lux;
+  int person_count;
+  int lux;
+  int lights_on;
+  int total_time;
+  int lights_initial_time;
+  int success;
 };
